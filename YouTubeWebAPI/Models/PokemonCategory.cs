00a9820c63e8125aaea4671ae87eb8a0a2ad076b
masuod <masuod@MASOUD-SURFACE->
2024-01-01 01:35:34 +0330

    Add project files.

---
+++ YouTubeWebAPI/Models/PokemonCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace YouTubeWebAPI.Models
+{
+    public class PokemonCategory
+    {
+        public int PokemonId { get; set; }
+        public int CategoryID { get; set; }
+        public Pokemon pokemon { get; set; }
+        public Category category { get; set; }
+    }
+}
