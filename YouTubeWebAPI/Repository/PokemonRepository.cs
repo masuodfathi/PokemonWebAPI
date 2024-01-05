8490c303443b11528f30c83bfc4dccb0e80b0f3c
masuodfathi <Masuod.fathi@gmail.com>
2024-01-06 00:14:43 +0330

    Repository

---
+++ YouTubeWebAPI/Repository/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using YouTubeWebAPI.Data;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Repository
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext dataContext) 
+        {
+            _context = dataContext;
+        }
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
