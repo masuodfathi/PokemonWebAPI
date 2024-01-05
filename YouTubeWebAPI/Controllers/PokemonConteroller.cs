d08bef5f09e43971df2c5519aec93bdba21092f7
masuodfathi <Masuod.fathi@gmail.com>
2024-01-06 00:14:10 +0330

    GetAllPokemons

---
+++ YouTubeWebAPI/Controllers/PokemonConteroller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class PokemonConteroller : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        public PokemonConteroller(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemons);
+        }
+    }
+}
