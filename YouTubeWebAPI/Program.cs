39921f52ddcf19caa82f27a165183d1192d9ce4e
masuodfathi <Masuod.fathi@gmail.com>
2024-01-06 00:15:05 +0330

    Scoped

--- YouTubeWebAPI/Program.cs
+++ YouTubeWebAPI/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using PokemonReviewApp;
+using YouTubeWebAPI.Data;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +10,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
-
+builder.Services.AddScoped<IPokemonRepository,PokemonRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DbContext>(options =>
+builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
