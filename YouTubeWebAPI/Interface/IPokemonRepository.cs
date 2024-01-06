51249797c5ecaed9d753613f026670120a8d1a57
masuodfathi <Masuod.fathi@gmail.com>
2024-01-06 17:15:31 +0330

    Interface Repository

--- YouTubeWebAPI/Interface/IPokemonRepository.cs
+++ YouTubeWebAPI/Interface/IPokemonRepository.cs
@@ -5,5 +5,9 @@ namespace YouTubeWebAPI.Interface
     public interface IPokemonRepository
     {
         ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokemonId);
+        bool PokemonExist(int pokemonId);
     }
 }
