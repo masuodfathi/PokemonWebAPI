03945b7b12c607b37f1dd80f5deb0ae6c88eb935
masuodfathi <Masuod.fathi@gmail.com>
2024-01-07 02:48:20 +0330

    category interface

---
+++ YouTubeWebAPI/Interface/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Interface
+{
+    public interface ICategoryRepository
+    {
+        ICollection<Category> GetCategories();
+        Category GetCategory(int id);
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+        bool CategoryExist(int Id);
+    }
+}
