59a7c03d73e383f7e668bbd556e25ce5a331fc4a
masuodfathi <Masuod.fathi@gmail.com>
2024-01-07 02:47:43 +0330

    pokemon controller

--- YouTubeWebAPI/Controllers/PokemonConteroller.cs
+++ YouTubeWebAPI/Controllers/PokemonConteroller.cs
@@ -14,7 +14,7 @@ namespace YouTubeWebAPI.Controllers
             _pokemonRepository = pokemonRepository;
         }
         [HttpGet]
-        [ProducesResponseType(200,Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
             var pokemons = _pokemonRepository.GetPokemons();
@@ -24,6 +24,34 @@ namespace YouTubeWebAPI.Controllers
                 return BadRequest(ModelState);
             }
             return Ok(pokemons);
+        }
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200,Type = typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExist(pokeId))
+            {
+                return NotFound();
+            }
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            return Ok(pokemon);
+        }
+        [HttpGet("{pokeId}/rating")]
+        [ProducesResponseType(200,Type = typeof(decimal))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonRating(int pokeId)
+        {
+            if(!_pokemonRepository.PokemonExist(pokeId))
+                return NotFound();
+            if(!ModelState.IsValid)
+                return BadRequest();
+            var rating = _pokemonRepository.GetPokemonRating(pokeId);
+            return Ok(rating);
         }
     }
 }
