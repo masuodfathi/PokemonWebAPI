45466f4d76a02b18f9530d78668b15222f309680
masuodfathi <Masuod.fathi@gmail.com>
2024-01-08 01:37:01 +0330

    review repository

---
+++ YouTubeWebAPI/Repository/ReviewRepository.cs
@@ -0,0 +1,34 @@
+﻿using YouTubeWebAPI.Data;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Repository
+{
+    public class ReviewRepository : IReviewRepository
+    {
+        private readonly DataContext _context;
+        public ReviewRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Review GetReview(int reviewId)
+        {
+            return _context.Reviews.Where(r => r.Id == reviewId).FirstOrDefault();
+        }
+
+        public ICollection<Review> GetReviews()
+        {
+            return _context.Reviews.ToList();
+        }
+
+        public ICollection<Review> GetReviewsOfPokemon(int pokemonId)
+        {
+            return _context.Reviews.Where(r => r.Pokemon.Id == pokemonId).ToList();
+        }
+
+        public bool ReviewExist(int reviewId)
+        {
+            return _context.Reviews.Any(r => r.Id == reviewId);
+        }
+    }
+}
