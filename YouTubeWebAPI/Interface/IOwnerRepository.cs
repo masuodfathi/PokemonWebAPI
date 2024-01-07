44a2ac0e1bd7f916ce4e0d8d78ee9beb791df02f
masuodfathi <Masuod.fathi@gmail.com>
2024-01-08 01:36:19 +0330

    owner interface

---
+++ YouTubeWebAPI/Interface/IOwnerRepository.cs
@@ -0,0 +1,14 @@
+﻿using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Interface
+{
+    public interface IOwnerRepository
+    {
+        ICollection<Owner> GetOwners();
+        Owner GetOwner(string firstName);
+        Owner GetOwner(int Id);
+        ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+        ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+        bool OwnerExist(int  ownerId);
+    }
+}
