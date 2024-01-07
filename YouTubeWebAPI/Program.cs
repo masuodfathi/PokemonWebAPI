3556ae8a49e135af24c1fd5c94ff69e6dfd04a02
masuodfathi <Masuod.fathi@gmail.com>
2024-01-07 18:30:05 +0330

    add scoped of srvices

--- YouTubeWebAPI/Program.cs
+++ YouTubeWebAPI/Program.cs
@@ -11,6 +11,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
 builder.Services.AddScoped<IPokemonRepository,PokemonRepository>();
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository,CountryRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
