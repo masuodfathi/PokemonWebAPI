b5e130a0fe40c7ffd8a9587711a47770e5e13a29
masuodfathi <Masuod.fathi@gmail.com>
2024-01-07 18:29:01 +0330

    Category repository

---
+++ YouTubeWebAPI/Repository/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using YouTubeWebAPI.Data;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly DataContext _context;
+        public CategoryRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public bool CategoryExist(int Id)
+        {
+            return _context.Categories.Any(c => c.Id == Id);
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.OrderBy(c => c.Name).ToList();
+        }
+
+        public Category GetCategory(int id)
+        {
+            return _context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        }
+
+        public ICollection<Pokemon> GetPokemonByCategory(int categoryId)
+        {
+            return _context.PokemonCategories.Where(pc => pc.CategoryID == categoryId).Select(pc => pc.Pokemon).ToList();
+        }
+    }
+}
