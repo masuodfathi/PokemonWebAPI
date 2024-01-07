66017bf75cdf21b71b5cc0e8e5c6846fc4c665dc
masuodfathi <Masuod.fathi@gmail.com>
2024-01-08 01:36:31 +0330

    review interface

---
+++ YouTubeWebAPI/Interface/IReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Interface
+{
+    public interface IReviewRepository
+    {
+        ICollection<Review> GetReviews();
+        Review GetReview(int reviewId);
+        ICollection<Review> GetReviewsOfPokemon(int pokemonId);
+        bool ReviewExist(int reviewId);
+    }
+}
