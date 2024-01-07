95a58541de9ade5e86edbee80f9b1d0eadc021bf
masuodfathi <Masuod.fathi@gmail.com>
2024-01-07 18:28:48 +0330

    Country interface

---
+++ YouTubeWebAPI/Interface/ICountryRepository.cs
@@ -0,0 +1,14 @@
+﻿using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Interface
+{
+    public interface ICountryRepository
+    {
+        ICollection<Country> GetCountries();
+        Country GetCountry(int id);
+        Country GetCountry(string name);
+        Country GetCountryByOwner(int  ownerId);
+        ICollection<Owner> GetOwnersFromACountry(int countryId);
+        bool CountryExsits(int id);
+    }
+}
