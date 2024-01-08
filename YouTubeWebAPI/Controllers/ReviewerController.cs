e78bb5cac783dac243a8264a3bbb55e766709141
masuodfathi <Masuod.fathi@gmail.com>
2024-01-09 01:48:56 +0330

    reviewer controller

---
+++ YouTubeWebAPI/Controllers/ReviewerController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewerController : Controller
+    {
+        private readonly IReviewerRepository _reviewerRepository;
+        public ReviewerController(IReviewerRepository reviewerRepository)
+        {
+            _reviewerRepository = reviewerRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200,Type = typeof(IEnumerable<Reviewer>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewers()
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var reviewers = _reviewerRepository.GetReviewers();
+            return Ok(reviewers);
+        }
+
+        [HttpGet("{reviewerId}")]
+        [ProducesResponseType(200, Type = typeof(Reviewer))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewerById(int reviewerId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(!_reviewerRepository.ReviewerExist(reviewerId))
+                return NotFound();
+
+            var reviewer = _reviewerRepository.GetReviewer(reviewerId);
+            return Ok(reviewer);
+        }
+
+        [HttpGet("{reviewerId}/reviews")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Review>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetReviewsByReviewerId(int reviewerId)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.ReviewerExist(reviewerId))
+                return NotFound();
+
+            var reviews = _reviewerRepository.GetReviewsByReviewer(reviewerId);
+            return Ok(reviews);
+        }
+    }
+}
