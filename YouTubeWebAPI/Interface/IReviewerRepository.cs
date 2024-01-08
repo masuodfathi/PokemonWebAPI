efffb25dc48fed2e01611696a0b11d0819533e16
masuodfathi <Masuod.fathi@gmail.com>
2024-01-09 01:49:11 +0330

    reviewer interface

---
+++ YouTubeWebAPI/Interface/IReviewerRepository.cs
@@ -0,0 +1,12 @@
+﻿using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Interface
+{
+    public interface IReviewerRepository
+    {
+        ICollection<Reviewer> GetReviewers();
+        Reviewer GetReviewer(int id);
+        ICollection<Review> GetReviewsByReviewer(int reviewerId);
+        bool ReviewerExist(int reviewerId);
+    }
+}
