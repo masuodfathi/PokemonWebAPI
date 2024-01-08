0a00af8d18bdbec4de64c1ee594bbcf9a1a18ebd
masuodfathi <Masuod.fathi@gmail.com>
2024-01-09 01:49:27 +0330

    reviewer repository

---
+++ YouTubeWebAPI/Repository/ReviewerRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YouTubeWebAPI.Data;
+using YouTubeWebAPI.Interface;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Repository
+{
+    public class ReviewerRepository : IReviewerRepository
+    {
+        private readonly DataContext _context;
+        public ReviewerRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public Reviewer GetReviewer(int id)
+        {
+            return _context.Reviewers.Where(r => r.Id == id).Include(re=>re.Reviews).FirstOrDefault();
+        }
+
+        public ICollection<Reviewer> GetReviewers()
+        {
+            return _context.Reviewers.ToList();
+        }
+
+        public ICollection<Review> GetReviewsByReviewer(int reviewerId)
+        {
+            return _context.Reviews.Where(r => r.Reviewer.Id == reviewerId).ToList();
+        }
+
+        public bool ReviewerExist(int reviewerId)
+        {
+            return _context.Reviewers.Any(r => r.Id == reviewerId);
+        }
+    }
+}
