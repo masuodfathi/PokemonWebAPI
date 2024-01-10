6aaac09f69c8985016962736de15238a7900ae5e
masuodfathi <Masuod.fathi@gmail.com>
2024-01-10 11:15:18 +0330

    mapper

---
+++ YouTubeWebAPI/Helper/MappingProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using YouTubeWebAPI.DTOs;
+using YouTubeWebAPI.Models;
+
+namespace YouTubeWebAPI.Helper
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<Category,CategoryDto>().ReverseMap();
+            CreateMap<Country,CountryDto>().ReverseMap();
+            CreateMap<Owner,OwnerDto>().ReverseMap();
+        }
+    }
+}
